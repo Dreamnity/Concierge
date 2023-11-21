a490c5f65c678e2053a5d2f8f8f89e8f007afaa3
5UP3R_D1N <42496449+superdinmc@users.noreply.github.com>
2023-11-21 20:42:14 +0700

    Update subdomainresolver.js

--- subdomainresolver.js
+++ subdomainresolver.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
         const url = new URL(req.url, `https://${req.headers.host}`)
         const sub = url.host.match(/(.+)\.dreamnity\.in/)[1];
         req.pipe(
-            http.get('http://cc.dreamnity.in/' + sub + url.search, res2 => {
+            http.get('http://localhost:8002/' + sub + '?' + req.url, res2 => {
                 res.writeHead(res2.statusCode, res2.headers);
                 res2.on('error',e=>res.end(e.message))
                 res2.pipe(res, { end: true })
