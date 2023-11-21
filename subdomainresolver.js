17da93b8c5be4c34a4efb018a655a2cc5047b75e
superdinmc <superdinmc@gmail.com>
2023-11-21 16:15:25 +0700

    better

---
+++ subdomainresolver.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    try {
+        const url = new URL(req.url, `https://${req.headers.host}`)
+        const sub = url.host.match(/(.+)\.dreamnity\.in/)[1];
+        req.pipe(
+            http.get('http://cc.dreamnity.in/' + sub + url.search, res2 => {
+                res.writeHead(res2.statusCode, res2.headers);
+                res2.on('error',e=>res.end(e.message))
+                res2.pipe(res, { end: true })
+            }).on('error',e=>res.end(e.message)),
+            { end: true }
+        )
+    }catch(e){res.end(e.message)}
+}).listen(8001)
+process.on('uncaughtException',e=>console.error(e))
